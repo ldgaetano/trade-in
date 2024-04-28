80ea421a7ab47a7e30b6946c526dac28173db09e
Luca D'Angelo <ldgaetano@protonmail.com>
2024-04-28 16:55:25 -0400

    Fix card-value-mapping contract builder.

--- src/main/scala/builders/contract_builders/CardValueMappingContractBuilder.scala
+++ src/main/scala/builders/contract_builders/CardValueMappingContractBuilder.scala
@@ -2,15 +2,22 @@ package builders.contract_builders
 
 import org.ergoplatform.appkit._
 import utils.TradeInUtils
+import special.sigma.GroupElement
+import configs.setup_config.TradeInSetupConfig
+import configs.report_config.TradeInReportConfig
 
-case class CardValueMappingContractBuilder() extends TokenIssuanceContractBuilder {
+case class CardValueMappingContractBuilder(
+    devPKGE: ErgoValue[GroupElement]
+) extends TokenIssuanceContractBuilder {
 
     override val script: String = TradeInUtils.CARD_VALUE_MAPPING_SCRIPT
 
     override def toErgoContract(implicit ctx: BlockchainContext): ErgoContract = {
 
         ctx.compileContract(
-            ConstantsBuilder.empty(),
+            ConstantsBuilder.create()
+                .item("$DevPKGE", devPKGE.getValue())
+                .build(),
             script
         )
 
@@ -18,4 +25,22 @@ case class CardValueMappingContractBuilder() extends TokenIssuanceContractBuilde
 
 }
 
-object CardValueMappingContractBuilder {}
+object CardValueMappingContractBuilder {
+
+    def apply(setupConfig: TradeInSetupConfig, reportConfig: TradeInReportConfig): CardValueMappingContractBuilder = {
+
+        val devPKGE: ErgoValue[GroupElement] = ErgoValue.of(Address.createEip3Address(
+            setupConfig.node.wallet.index,
+            setupConfig.node.networkType,
+            SecretString.create(setupConfig.node.wallet.mnemonic),
+            SecretString.create(setupConfig.node.wallet.password),
+            false
+        ).getPublicKeyGE())
+
+        new CardValueMappingContractBuilder(
+            devPKGE
+        )
+
+    }
+
+}
